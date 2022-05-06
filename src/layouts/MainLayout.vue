cfe19ddda9c2b96c33f760a14ecbb459ce95aa1d
hiram.rubio <floreshiram03@gmail.com>
2022-05-05 21:13:07 -0500

    web development diagnostic v1

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,92 @@
+<template>
+  <q-layout view="hhh lPr lff">
+    <q-header>
+      <q-toolbar class="bg-white text-primary row justify-center">
+        <div style="width: 800px" class="row justify-between">
+          <div class="row">
+            <div class="text-bold">Gengar Guide</div>
+          </div>
+          <div class="row q-col-gutter-x-lg">
+            <div>GHASTLY</div>
+            <div>HAUNTER</div>
+            <div>GENGAR</div>
+            <div>MEGA GENGAR</div>
+          </div>
+        </div>
+      </q-toolbar>
+    </q-header>
+    <q-footer class="text-center q-py-md" style="font-size: 12px"
+      >&copy;2022 Skimlord</q-footer
+    >
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import EssentialLink from "components/EssentialLink.vue";
+
+const linksList = [
+  {
+    title: "Docs",
+    caption: "quasar.dev",
+    icon: "school",
+    link: "https://quasar.dev",
+  },
+  {
+    title: "Github",
+    caption: "github.com/quasarframework",
+    icon: "code",
+    link: "https://github.com/quasarframework",
+  },
+  {
+    title: "Discord Chat Channel",
+    caption: "chat.quasar.dev",
+    icon: "chat",
+    link: "https://chat.quasar.dev",
+  },
+  {
+    title: "Forum",
+    caption: "forum.quasar.dev",
+    icon: "record_voice_over",
+    link: "https://forum.quasar.dev",
+  },
+  {
+    title: "Twitter",
+    caption: "@quasarframework",
+    icon: "rss_feed",
+    link: "https://twitter.quasar.dev",
+  },
+  {
+    title: "Facebook",
+    caption: "@QuasarFramework",
+    icon: "public",
+    link: "https://facebook.quasar.dev",
+  },
+  {
+    title: "Quasar Awesome",
+    caption: "Community Quasar projects",
+    icon: "favorite",
+    link: "https://awesome.quasar.dev",
+  },
+];
+
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "MainLayout",
+
+  setup() {
+    const leftDrawerOpen = ref(false);
+
+    return {
+      essentialLinks: linksList,
+      leftDrawerOpen,
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
+      },
+    };
+  },
+});
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,77 @@
+<template>
+  <section class="row justify-center bg-primary">
+    <div style="max-width: 800px" class="text-center q-py-xl text-white">
+      <img src="~/assets/gengar.png" alt="gengar" style="width: 150px" />
+      <h6 class="q-mb-sm text-bold">Gengar</h6>
+      <p style="margin: 0">Best pokemon in the entire world.</p>
+      <p></p>
+    </div>
+  </section>
+  <section class="row justify-center bg-seconday">
+    <div style="max-width: 800px" class="row q-col-gutter-x-lg q-py-xl section">
+      <div class="col-sm-12 col-md-4">
+        <h6 class="q-mb-lg text-bold">Gengar rules the world</h6>
+        <p style="font-size: 12px">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam amet
+          ut eos. Consequuntur similique laboriosam delectus debitis qui
+          architecto commodi nisi consectetur veniam! Blanditiis molestiae
+          quisquam commodi voluptatum officia cupiditate!
+        </p>
+        <p style="font-size: 12px">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam amet
+          ut eos. Consequuntur similique laboriosam delectus debitis qui
+          architecto commodi nisi consectetur veniam! Blanditiis molestiae
+          quisquam commodi voluptatum officia cupiditate!
+        </p>
+        <p style="font-size: 12px">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam amet
+          ut eos. Consequuntur similique laboriosam delectus debitis qui
+          architecto commodi nisi consectetur veniam! Blanditiis molestiae
+          quisquam commodi voluptatum officia cupiditate!
+        </p>
+      </div>
+      <div class="col-sm-12 col-md-4">
+        <h6 class="q-mb-lg text-bold">Who's Gengar?</h6>
+        <div class="text-center">
+          <img src="~/assets/ghosts.png" alt="" style="width: 200px" />
+        </div>
+        <p style="font-size: 12px">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam amet
+          ut eos. Consequuntur similique laboriosam delectus debitis qui
+          architecto commodi nisi consectetur veniam! Blanditiis molestiae
+          quisquam commodi voluptatum officia cupiditate!
+        </p>
+      </div>
+      <div class="col-sm-12 col-md-4">
+        <h6 class="q-mb-lg text-bold">Ghost Pokemons rule</h6>
+        <p style="font-size: 12px">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam amet
+          ut eos. Consequuntur similique laboriosam delectus debitis qui
+          architecto commodi nisi consectetur veniam! Blanditiis molestiae
+          quisquam commodi voluptatum officia cupiditate!
+        </p>
+        <p style="font-size: 12px">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam amet
+          ut eos. Consequuntur similique laboriosam delectus debitis qui
+          architecto commodi nisi consectetur veniam! Blanditiis molestiae
+          quisquam commodi voluptatum officia cupiditate!
+        </p>
+        <p style="font-size: 12px">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam amet
+          ut eos. Consequuntur similique laboriosam delectus debitis qui
+          architecto commodi nisi consectetur veniam! Blanditiis molestiae
+          quisquam commodi voluptatum officia cupiditate!
+        </p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script></script>
+<style scoped>
+@media (max-width: 1000px) {
+  .section {
+    padding: 30px;
+  }
+}
+</style>
